c95b2c906ce3e5952f549f02563c1cb311ef5c6c
Muhammad Kashif <mk.kashif.se@gmail.com>
2022-08-14 23:48:08 +0500

    code clean up

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const product = require("./api/product");
 
-const hello = require("./api/hello");
+app.use(express.json({ extended: false }));
 
+app.use("/api/product", product);
 
-
-app.use("/api/hello", hello);
-
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
