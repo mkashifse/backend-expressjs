3ab15cc6dbb583157d18b1cd7a88c376b337931b
Muhammad Kashif <mk.kashif.se@gmail.com>
2022-08-16 14:02:18 +0500

    added

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product");
-
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+const fs = require('fs');
+
+// default responce
+app.get("/", (req, resp) => {
+  resp.send("Welcome to the App");
+});
+
+app.get("/get-all-users", (req, resp) => {
+  const users = require("./db/users.json");
+  resp.send(users);
+});
+
+app.post("/add-new-user", (req, resp) => {
+  const users = require("./db/users.json");
+  const data = req.body;
+  console.log(data, req.body);
+  users.push(data);
+  fs.writeFileSync("./db/users.json", JSON.stringify(users));
+  resp.send(users);
+});
+
+app.get("/get-all-posts", (req, resp) => {
+  const posts = require("./db/posts.json");
+  resp.send(posts);
+});
+
+app.get("/get-some-posts", (req, resp) => {
+  const posts = require("./db/posts.json"); // 100
+  const somePosts = posts.slice(0, req.query.size);
+  resp.send(somePosts);
+});
+
+app.get("/get-posts", (req, resp) => {
+  const posts = require("./db/posts.json");
+  const { userId } = req.query;
+  const filteredPosts = posts.filter((post)=> +post.userId === +userId);
+  resp.send(filteredPosts);
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
